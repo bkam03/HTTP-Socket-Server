75fdbd471dce1692af53657a04c2807ae13d840c
BK <bkam03@yahoo.com>
2017-07-06 19:24:53 -1000

    fixed async issue, now returns website

--- server.js
+++ server.js
@@ -9,38 +9,35 @@ myServer.listen( 8080, function(){
   myServer.on( 'connection', function( socket ){
     socket.setEncoding( 'utf8' );
     socket.on( 'data', function( chunk ){
-      console.log( chunk );
+      console.log( `chunk\n${ chunk }` );
       let chunkAsArray = chunk.split( ' ' );
       let requestMethod = chunkAsArray[ 0 ];
       let requestPath = `.${ chunkAsArray[ 1 ] }`;
-
       var htmlBody = '';
-      //read input
-      console.log( requestPath );
-      fs.readFile( requestPath, ( err, data ) => {
-        if( err ) throw err;
-        console.log( `@${ data }` );
-        htmlBody = data;
-        console.log( htmlBody );
-      } );
+
+      console.log( requestPath + '\n' );
+      //try {
+        fs.readFile( requestPath, 'utf8', ( err, data ) => {
+          if( err ){
+            fs.readFile( './404.html', 'utf8', ( err, errorSite ) => {
+              htmlBody = errorSite;
+            } );
+          } else {
+            htmlBody = data;
+          }
+
+          console.log( `-${ htmlBody }-` );
 
 
-
-      //output
-
-      let date = new Date().toUTCString();
+          let date = new Date().toUTCString();
 
 
-      socket.write( `HTTP/1.1 200 OK\nServer: testServer\nDate: ${ date }\n\n${ htmlBody }` );
+          socket.write( `HTTP/1.1 200 OK\nServer: testServer\nDate: ${ date }\n\n${htmlBody}` );
+          //socket.write( `HTTP/1.1 200 OK\nServer: testServer\nDate: ${ date }\n\n${ htmlBody }` );
 
-      socket.end();
+          socket.end();
+       } );
     } );
-
-
-
-    //trasmit a hardcoded, in memory html body for each route
-
-
   } );
 
   //if path is not found in routes, return a 404 and output some text/html content
