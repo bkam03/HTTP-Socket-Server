a245bcfc77e9fe3efaf424eb3f6455c7f09c63ba
BK <bkam03@yahoo.com>
2017-07-08 11:24:25 -1000

    only reply body outputs

--- client.js
+++ client.js
@@ -1,38 +1,42 @@
-/*
-The node command requires a single argument, the host and uri to request a resource from
-example: www.devleague.com/apply
-Transmit 'standard' HTTP Headers to the server
-Wait for a response from the server
-When the server responds, display the response message body to the terminal
-If the node client is run with no arguments, display a "help/usage" message that explains how to use your client, including all available options
-example usage:
-
-node client.js www.devleague.com
-*/
 
 const net = require( 'net' );
 const process = require( 'process' );
 
-let host = process.argv[ 2 ];
-console.log( host );
+var url = process.argv[ 2 ].split( '/' );
+var host = url[ 0 ];
+var uri = url[ 1 ];
+var headerProperties = {};
 
 
-if( host == undefined ){
-    console.log( 'help/usage message' );
+if( host === undefined ){
+    console.log( 'type "node client.js urlThingHere" for the client to access that website.' );
 } else {
-  console.log( 'in the else' );
   var client = net.createConnection( 80, host, function(){
-    console.log( 'in the client' );
     client.setEncoding( 'utf8' );
 
     client.on( 'data', function( data ){
-      console.log( data );
-      client.end();
-    } );
-    client.write( 'GET / HTTP/1.1\r\n\r\n' );
-  } );
+      headerArray = data.split('\n');
+      //console.log( headerArray );
+      for( var i = 1; i < headerArray.length; i++ ){
+        let keyPairArray = headerArray[ i ].split( ' ' );
+        let key = keyPairArray[ 0 ];
+        key = key.slice( 0, key.length - 1 );
+        let property = keyPairArray[ 1 ];
+        headerProperties[ key ] = keyPairArray[ 1 ];
+      }
 
-    //client.write( 'GET / HTTP/1.1\r\nHost: www.example.com\r\nConnection: close\r\n\r\n' );
+      let htmlBody = data.split( '\r\n\r\n' )[ 1 ];
+      //console.log( `@@${ htmlBody }` );
+      //let htmlBody = data;
+      process.stdout.write( htmlBody.toString() );
+      //client.end();
+    } );
+
+    let date = new Date().toUTCString();
+
+    client.write( `GET / HTTP/1.1\r\nHost: ${ host }\r\nDate: ${ date }\r\nUser-Agent: Poor Man Browser\r\nConnection: close\r\n\r\n` );
+    //client.write( 'GET /${ uri } HTTP/1.1\r\n\r\n' );
+  } );
 }
 
 
